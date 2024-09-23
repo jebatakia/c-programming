b47ed1f7166e156faa2c5adb13c5f3a4dcfe71ef
Jeba Tarannum Takia <jeba.contact@gmail.com>
2024-09-23 14:39:30 +0600

    escape sequence

---
+++ c/escape sequence.c
@@ -0,0 +1,22 @@
+/*
+Jeba Tarannum Takia
+Northern University Bangladesh
+Dhaka, Bangladesh
+jeba.contact@gmail.com
+01740878233
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    printf("Jeba Trannum Takia\n");
+    printf("Northern University Bangladesh\n");
+    printf("Dhaka, Bangladesh\n");
+    printf("jeba.contact@gmail.com\n");
+    printf("01740878233\n");
+
+
+    return 0;
+}
+
