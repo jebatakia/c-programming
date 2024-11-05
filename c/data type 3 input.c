484c7bbb1ec6a9bb83f42608be32f49d23d9e088
Jeba Tarannum Takia <jeba.contact@gmail.com>
2024-11-05 13:44:40 +0600

    data type 3 input

---
+++ c/data type 3 input.c
@@ -0,0 +1,13 @@
+/*Write a program that takes a integer
+and print that number*/
+
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("please enter an integer= ");
+    scanf("%d",&num);
+    printf("You have pressed=%d\n", num);
+
+
+}
