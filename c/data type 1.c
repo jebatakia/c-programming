0937fcfe235441ccf2eb23b8de34d18bdd743d38
Jeba Tarannum Takia <jeba.contact@gmail.com>
2024-11-05 12:53:54 +0600

    data type 1

---
+++ c/data type 1.c
@@ -0,0 +1,15 @@
+/*An integer variable num that will contain 10,
+Wite a program that prints integer num*/
+
+#include<stdio.h>
+int main()
+{
+    int num1=10;
+    int num2=20;
+
+    printf("Number is %d\n",num1);
+    printf("number2 is %d",num2);
+
+    return 0;
+}
+
